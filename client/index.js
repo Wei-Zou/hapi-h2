8dbd2f4d487ab43f8c02738045f122dc339351d2
Wei-Zou <wz.weizou@gmail.com>
2018-08-05 21:42:43 -0700

    render a Tile correctly

--- client/index.js
+++ client/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Tile from './Tile.jsx';
+
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { name: 'Medium' };
-  }
-
-  handleChange(e) {
-    this.setState({ name: e.target.value });
   }
 
   render() {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Welcome</h1>
-        <p>Hello {this.state.name}</p>
-        <input onChange={this.handleChange} defaultValue={this.state.name}/>
+        <Tile src="./images/0-0.jpeg"/>
       </div>
     )
   }
