091bb5c76272236807dd9601b3b94634980e1445
Wei-Zou <wz.weizou@gmail.com>
2018-08-05 22:27:58 -0700

    serve image in h2 and use iframe to compare

--- h2-server.js
+++ h2-server.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const Hapi = require("hapi");
+const vision = require('vision');
+const inert = require('inert');
 const fs = require('fs');
 const http2 = require('http2');
+
+const { setupTemplates } = require('./templates/setup-templates');
 
 const serverOptions = {
   key: fs.readFileSync('./certs/privateKey.pem'),
@@ -14,19 +18,53 @@ const listener = http2.createSecureServer(serverOptions);
 
 const server = Hapi.server({
   listener,
-  port: 3000,
+  port: 3001,
   host: 'localhost',
 });
 
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, h) => {
-    return 'world\n';
-  },
-});
-
 const init = async () => {
+  await server.register(vision);
+  await server.register(inert);
+  setupTemplates(server);
+
+  server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: (request, h) => {
+      return 'world/n';
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/scripts/{file}',
+    config: {
+      handler: async (request, h) => {
+        return h.file(`./public/scripts/${request.params.file}`);
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/images/{file}',
+    config: {
+      handler: async (request, h) => {
+        return h.file(`./public/images/${request.params.file}`);
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/tiles',
+    config: {
+      handler: async (request, h) => {
+        return h.view('index');
+      },
+    },
+  });
+
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 };
